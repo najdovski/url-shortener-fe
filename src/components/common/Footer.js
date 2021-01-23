9a4914897eb8d565ad590736f9b743b88a311922
Kristijan Najdovski <kristijannds@gmail.com>
2021-01-23 03:27:24 +0100

    footer email

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -5,7 +5,10 @@ const Footer = () => {
   return (
     <footer className="container-fluid text-white bg-dark py-2 px-5 mt-auto">
       <div className="row justify-content-between">
-        <div className="col-12 text-center text-sm-right">
+        <div className="col email-contact">
+          <small className="text-gray-muted mr-2">contact@axe.lu</small>
+        </div>
+        <div className="col-auto text-center text-sm-right">
           <small className="mr-2">Copyright &#169; <span className="font-weight-bold">{(new Date().getFullYear())}</span> </small>
           <small className="text-primary text-nowrap font-weight-bold"><Link to="/">{process.env.REACT_APP_NAME}</Link></small>
         </div>
